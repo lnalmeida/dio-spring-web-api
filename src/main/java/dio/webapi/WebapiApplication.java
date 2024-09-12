9a1bbe40494830552132d2007cfa7e995dba80de
LUIZ NUNES DE ALMEIDA JUNIOR <l.n.almeida.ti@gmail.com>
2024-09-12 11:56:30 -0300

    Added Open API documentation

--- src/main/java/dio/webapi/WebapiApplication.java
+++ src/main/java/dio/webapi/WebapiApplication.java
@@ -1,9 +1,30 @@
 package dio.webapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring web API",
+				version = "1.0.0",
+				description = "API feita com fins didáticos, durante o bootcampo DIO/Claro de Spring boot + Java. Sujeita a modificações.",
+				termsOfService = "Open source - https://github.com/lnalmeida/dio-spring-web-api.git",
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				),
+				contact = @Contact(
+						name = "Luiz Almeida",
+						email = "l.n.almeida.dev@outlook.com",
+						url = "https://lna-next-js-protifolio.vercel.app/"
+				)
+		)
+)
 public class WebapiApplication {
 
 	public static void main(String[] args) {
